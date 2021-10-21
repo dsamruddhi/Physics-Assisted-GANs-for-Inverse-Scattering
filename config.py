71491f195d80570c7590c574b30d8761b22a96aa
DESHMUKH <ssdeshmukh@ust.hk>
2021-10-21 21:21:03 +0800

    Updated config file

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config:
 
-    EXPERIMENT_NAME = "test"
-    MODEL_NAME = 'PhyGAN'
+    EXPERIMENT_NAME = ""
+    MODEL_NAME = ""
     EPOCHS = 200
 
-    model_path = rf"D:\OneDrive - HKUST Connect\MPHIL RESEARCH\MODELS\{MODEL_NAME}"
-    data_path = r"D:\OneDrive - HKUST Connect\MPHIL RESEARCH\DATA\TWO_OBJECTS"
-    oos_data_path = r"D:\OneDrive - HKUST Connect\MPHIL RESEARCH\DATA\zTEST_SIMULATION"
+    model_path = ""
+    data_path = ""
+    oos_data_path = ""
