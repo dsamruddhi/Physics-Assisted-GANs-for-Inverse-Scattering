c027be5770bb5f0ce7bfb56dc95bb38fe01fbeb6
Samruddhi Deshmukh <dsamruddhi21@gmail.com>
2021-10-24 15:35:37 +0800

    Added config file

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+
+    EXPERIMENT_NAME = ""
+    MODEL_NAME = ""
+    EPOCHS = 50
+    N_CRITIC = 5
+
+    MODEL = ""
+
+    model_path = ""
+    data_path = ""
+    oos_data_path = ""
+    results_path = ""
